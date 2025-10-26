8b04162442309e32620ebb9daef6650405eec1f7
chedrick343 <chedrickda014@estudiantec.cr>
2025-10-26 15:39:45 -0600

    feat: creando la tabla persona

--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -2,3 +2,13 @@ CREATE TABLE tipo_identificacion (
     id_identificacion SERIAL PRIMARY KEY,
     nombre_identificacion VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE persona (
+    id_persona SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    primer_apellido VARCHAR(100) NOT NULL,
+    segundo_apellido VARCHAR(100),
+    numero_identificacion VARCHAR(50) NOT NULL,
+    id_identificacion INT NOT NULL REFERENCES tipo_identificacion(id_identificacion),
+    numero_telefono VARCHAR(20)
+);
