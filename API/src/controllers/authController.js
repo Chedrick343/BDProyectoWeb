1cfa06f7d59cc73bda2c98f17f2a4d13192cb33f
chedrick343 <chedrickda014@estudiantec.cr>
2025-11-01 10:25:01 -0600

    feat: agregando el Api key a la bd y creando la validación

--- API/src/controllers/authController.js
+++ API/src/controllers/authController.js
@@ -1,24 +1,55 @@
-// src/controllers/authController.js
-export const login = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(`Intento de login para el email: ${email}`);
-  console.log(`Password recibido: ${password}`);
+import { pool } from '../config/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-  // Aquí luego ejecutarás el SP de autenticación
-  if (email === 'test@banco.com' && password === '1234') {
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
+
+export const login = async (req, res) => {
+  try{
+    const { username_or_email, email, username, password } = req.body;
+    const identifier = (username_or_email || email || username || '').toString().trim();
+    if (!identifier || !password) {
+      return res.status(400).json({ message: 'Faltan credenciales', status: 'error' });
+    }
+    const sql = 'SELECT * FROM sp_auth_user_get_by_username_or_email($1)';
+    const { rows } = await pool.query(sql, [identifier]);
+
+
+    if (!rows || rows.length === 0) {
+      return res.status(401).json({ status: 'error', message: 'Credenciales inválidas.' });
+    }
+    const user = rows[0];
+    const passwordHash = user.contrasena_hash;
+
+    const match = await bcrypt.compare(password, passwordHash);
+    if (!match) {
+      return res.status(401).json({ status: 'error', message: 'Credenciales inválidas.' });
+    }
+
+    const payload = {
+      userId: user.user_id,
+      role: user.rol_nombre
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
     return res.status(200).json({
-      message: 'Login exitoso (simulado)',
-      token: 'jwt_simulado_123'
+      status: 'success',
+      message: 'Autenticación correcta.',
+      data: {
+        token,
+        expiresIn: JWT_EXPIRES_IN
+      }
+    });
+
+  }catch (error) {
+    console.error('Error en login:', err);
+    // No devuelvas el error de BD al cliente
+    return res.status(500).json({
+      status: 'error',
+      message: 'Ocurrió un error al procesar la autenticación.'
     });
   }
-
-  res.status(401).json({ message: 'Credenciales inválidas' });
-};
-
-export const forgotPassword = async (req, res) => {
-  res.status(200).json({ message: 'Solicitud de recuperación enviada (mock)' });
-};
-
-export const resetPassword = async (req, res) => {
-  res.status(200).json({ message: 'Contraseña restablecida (mock)' });
 };
--- API/src/middlewares/apiKeyAuth.js
+++ API/src/middlewares/apiKeyAuth.js
@@ -1,23 +1,35 @@
 // src/middlewares/apiKeyAuth.js
 import dotenv from 'dotenv';
+import { pool } from "../config/db.js";
 dotenv.config();
 
-export const apiKeyAuth = (req, res, next) => {
-  const apiKey = req.headers['x-api-key'];
+export const apiKeyAuth =  async (req, res, next) => {
+  try {
+    const apiKey = req.headers['x-api-key'];
 
-  if (!apiKey) {
-    return res.status(401).json({
-      status: 'error',
-      message: 'Falta la API Key en los headers (x-api-key).'
+    if (!apiKey) {
+      return res.status(401).json({
+        status: 'error',
+        message: 'Falta la API Key en los headers (x-api-key).'
+      });
+    }
+    const query = "SELECT sp_api_key_is_active($1) AS is_active";
+    const { rows } = await pool.query(query, [apiKey]);
+
+    const isActive = rows.length > 0 ? rows[0].is_active : false;
+
+    if (!isActive) {
+      return res.status(403).json({
+        status: "error",
+        message: "API Key inválida o inactiva.",
+      });
+    }
+    next()
+  }catch (error) {
+    console.error("Error en verifyApiKey:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error interno al validar la API Key.",
     });
   }
-
-  if (apiKey !== process.env.API_KEY) {
-    return res.status(403).json({
-      status: 'error',
-      message: 'API Key inválida o no autorizada.'
-    });
-  }
-
-  next();
 };
